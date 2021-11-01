39e82705e2ebda7dba28d5c6db96c798b94ee7a0
Duong <Duongchauv@gmail.com>
2021-10-31 20:39:20 -0500

    Responsive adjustments to service section

--- src/Components/Card/CardElements.js
+++ src/Components/Card/CardElements.js
@@ -1,61 +1,58 @@
 import styled from 'styled-components';
 
 export const CardImage = styled.img`
-    width:4.688rem;
-    height:auto;
+    width:4.7rem;
 `
-
 export const CardHeading = styled.p`
-    font-size: 1.5rem;
+    font-size: 1.4rem;
     padding:10px 0;
+    font-weight:500;
 `
-
 export const CardLink = styled.p`
     font-family: 'Open Sans';
+    font-weight:300;
 `
 export const CardContainer = styled.div`
     display:flex;
     flex-direction: column;
     justify-content: center;
-    box-shadow: 2px 2px 12px 3px rgba(0, 0, 0, 0.2);
     align-items: center;
-    width:12.5rem;
+    box-shadow: 2px 2px 12px 3px rgba(0, 0, 0, 0.2);
+    width:13rem;
     border-radius: 20px;
     background-color: white;
     height:15rem;
     color:black;
     transition: 0.3s ease-in-out;
+    justify-self:center;
+    align-self:center;
 
     &:hover{
         background-color:#f5a8a0;
         color:black;
     }
     @media (max-width:1200px){
-        width:11rem; 
-        height:13rem;
+        width:12rem; 
+        height:14rem;
+        ${CardHeading}{font-size:1.35rem}
+        ${CardLink}{font-size:.98rem;}
+        ${CardImage}{width:4.5rem;}
     }
     @media (max-width:992px){
-        width:10rem;
-        height:13rem;
+        width:9rem;
+        height:12rem;
         margin:0 10px;
         ${CardHeading}{font-size:1.3rem}
-        ${CardLink}{font-size:.9rem;}
-        ${CardImage}{}
+        ${CardLink}{font-size:.96rem;}
+        ${CardImage}{width:4.3rem;}
     }
     @media (max-width:768px){
-        width:13.5rem;
-        height:8rem;
-        margin:10px;
+        width:88%;
+        height:76%;
         border-radius: 10px;
-        ${CardHeading}{font-size:1rem; padding:3px 0;}
-        ${CardLink}{font-size:.9rem;}
-        ${CardImage}{width:3rem;}
-    }
-    @media (max-width:576px){
-        width:10rem;
-        height:7rem;
-        ${CardHeading}{font-size:.9rem; padding:0}
-        ${CardLink}{font-size:.8rem}
-        ${CardImage}{width:2.8rem;}
+        justify-self:center;
+        ${CardHeading}{font-size:1rem; padding:7px 0;}
+        ${CardLink}{display:none;}
+        ${CardImage}{height:50%;}
     }
 `
--- src/Components/Service/ServiceElements.js
+++ src/Components/Service/ServiceElements.js
@@ -1,43 +1,32 @@
 import styled from 'styled-components'
 
-export const TextSubHeading=styled.h4`
-    color:#fc3f00;
-`
 export const TextContainer = styled.div`
     display:flex;
     justify-content: center;
     align-items: center;
-    align-self:flex-start;
     grid-area: text;
-    width:100%;
-    height:100%;
 `
 export const TextWrapper = styled.div`
     display:flex;
-    width:90%;
-    max-width:400px;
-    height:auto;
+    max-width:360px;
     flex-direction: column;
-    justify-content: center;
-    align-items:flex-start;
 `
-
+export const TextSubHeading=styled.h4`
+    color:#f2674d;
+`
 export const TextHeader = styled.h2`
     color:black;
-    padding:5px 0 0 0; 
+    padding:5px 0 0 0;
     font-size: 2.1rem;
 `
-
 export const Text = styled.p`
     padding:20px 0;
     color:black;
+    font-size:1.2rem;
     font-family:'Open Sans';
-    font-size:1.1rem;
 `
 export const CardWrapper = styled.div`
     display:grid;
-    width:auto;
-    height:auto;
     grid-area: card;
     grid-template-columns: 12.5rem 12.5rem;
     grid-gap: 30px;
@@ -46,25 +35,22 @@ export const CardWrapper = styled.div`
     align-content: center;
 `
 export const ServiceContainer = styled.div`
-    width:80%;
+    width:90%;
     height:100vh;
     max-width: 75rem;
     margin: 0 auto;
-    padding-top: 40px;
     display:grid;
     grid-template-columns: 1fr 1fr;
     grid-template-areas:'card text';
-    justify-content:center;
-    align-content: center;
     border-bottom: 3px solid #d9d9d9;
 
     @media (max-width:1200px){
         ${CardWrapper}{
-            grid-gap:7px;
+            grid-gap:15px;
         }
-        ${TextSubHeading}{font-size:.98rem;}
+        ${TextSubHeading}{font-size:.98rem}
         ${TextHeader}{font-size:2rem}
-        ${Text}{font-size:1.05rem}
+        ${Text}{font-size:1.15rem}
         ${TextWrapper}{max-width:340px}
     }
     @media (max-width:992px){
@@ -72,39 +58,43 @@ export const ServiceContainer = styled.div`
             display:flex;
             justify-content: center;
             align-items: center;
-            margin-bottom:40px;
-            margin-left:0;
+            margin-bottom:50px;
+            align-self:flex-end;
         }
         ${TextSubHeading}{font-size:.96rem}
         ${TextHeader}{font-size:1.9rem}
-        ${Text}{font-size:1rem}
-        ${TextWrapper}{max-width:800px}
+        ${Text}{font-size:1.1rem}
+        ${TextWrapper}{max-width:600px; height:100%; align-self:end;}
 
-        grid-template-rows:auto auto;
+        grid-template-rows:1fr 1fr;
         grid-template-columns: 1fr;
         grid-template-areas:'card' 'text';
-        justify-content: center;
         align-content: center;
-        grid-gap: 20px;
     }
     @media (max-width:768px){
+
+        grid-template-columns: 1fr;
+        grid-template-rows:1fr 1fr;
+
         ${TextSubHeading}{font-size:.94rem}
         ${TextHeader}{font-size:1.8rem}
-        ${Text}{font-size:.95rem}
-        ${TextWrapper}{max-width:700px}
+        ${Text}{font-size:1.05rem}
+        ${TextWrapper}{max-width:500px; padding-top:30px;}
         ${CardWrapper}{
             display:grid;
-            grid-template-columns: 1fr 1fr;
+            align-self:end;
+            width:100%;
+            height:70%;
+            grid-template-columns: 40% 40%;
             grid-template-rows: 1fr 1fr;
-            justify-content: center;
-            align-content: center;
-            margin:0 auto 20px auto;
+            grid-gap:0;
+            margin:0;
         }
     }
     @media (max-width:576px){
         ${TextSubHeading}{font-size:.92rem}
         ${TextHeader}{font-size:1.7rem}
-        ${Text}{font-size:.9rem}
-        ${TextWrapper}{max-width:400px}
+        ${Text}{font-size:1rem}
+        ${TextWrapper}{max-width:88%}
     }
 `
